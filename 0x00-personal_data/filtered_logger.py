640565018de4c840c197f90ef4ce423fdcdd4044
ahmed <rifkiahmed.91@gmail.com>
2024-02-07 22:26:46 +0100

    Set default values for the database credentials

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -31,7 +31,7 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     """Returns a connector to the database"""
     kwargs = {
         "host": os.getenv("PERSONAL_DATA_DB_HOST", "localhost"),
-        "database": os.getenv("PERSONAL_DATA_DB_NAME"),
+        "database": os.getenv("PERSONAL_DATA_DB_NAME", "holberton"),
         "user": os.getenv("PERSONAL_DATA_DB_USERNAME", "root"),
         "password": os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
     }
