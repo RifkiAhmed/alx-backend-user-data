4d4e00b5d282e7cf3c9e881cfbd4cff80bc3f58f
ahmed <rifkiahmed.91@gmail.com>
2024-02-07 18:44:57 +0100

    Add type-annotation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -20,12 +20,12 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.__fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """Filter values in incoming log records"""
-        record.msg = filter_datum(self.__fields, self.REDACTION,
+        record.msg: str = filter_datum(self.__fields, self.REDACTION,
                                   record.getMessage(), self.SEPARATOR)
         return super().format(record)
