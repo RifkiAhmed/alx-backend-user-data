59c1affb0c02cd0ef55141fbe23c4523614f3041
ahmed <rifkiahmed.91@gmail.com>
2024-02-07 18:00:10 +0100

    Returns the log message obfuscated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,7 +5,7 @@ from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
+                 separator: str):
     """Returns the log message obfuscated"""
     return re.sub(fr'({"|".join(fields)})=[^{separator}]+',
                   lambda field: f'{field.group(1)}={redaction}', message)
