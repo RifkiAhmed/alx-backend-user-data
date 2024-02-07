a43bc0f247f7afd116de74ad832470d29a93b058
ahmed <rifkiahmed.91@gmail.com>
2024-02-07 20:15:00 +0100

    Create function that returns a Logger object

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,12 +4,23 @@ import logging
 import re
 from typing import List
 
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """Returns the log message obfuscated"""
     return re.sub(fr'({"|".join(fields)})=[^{separator}]+',
                   lambda field: f'{field.group(1)}={redaction}', message)
+
+
+def get_logger() -> logging.Logger:
+    """Return a Logger object"""
+    logger = logging.getLogger("user_data", 20)
+    logger.propagate = False
+    handler = logging.StreamHandler(RedactingFormatter())
+    logger.addHandler(handler)
+    return logger
 
 
 class RedactingFormatter(logging.Formatter):
