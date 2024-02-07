2d6bb7417be77a22b1ac62742c20089f995a4530
ahmed <rifkiahmed.91@gmail.com>
2024-02-07 18:00:57 +0100

    Add type-annotation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,7 +5,7 @@ from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str):
+                 separator: str) -> str:
     """Returns the log message obfuscated"""
     return re.sub(fr'({"|".join(fields)})=[^{separator}]+',
                   lambda field: f'{field.group(1)}={redaction}', message)
