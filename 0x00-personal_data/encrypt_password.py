1964149cd1c3edf519b59aafb635701346edf09c
ahmed <rifkiahmed.91@gmail.com>
2024-02-08 15:48:23 +0100

    Remove a blank line

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,7 +5,6 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     """Returns hashed password"""
-
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
