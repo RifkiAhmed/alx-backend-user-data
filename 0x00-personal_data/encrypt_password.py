6fcb0d229708fce4dd64f82f77bf2dc7ac7b5f02
ahmed <rifkiahmed.91@gmail.com>
2024-02-08 02:50:24 +0100

    Add type annotation

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,6 +11,6 @@ def hash_password(password: str) -> bytes:
     return hashed_password
 
 
-def is_valid(hashed_password: bytes, password: str):
+def is_valid(hashed_password: bytes, password: str) -> bool:
     """Check i the password matches the hashed password"""
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
