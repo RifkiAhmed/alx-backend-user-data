35897d4cf3be176b523cc6ae6d737472380e1591
ahmed <rifkiahmed.91@gmail.com>
2024-02-08 02:49:17 +0100

    Add hash_password and is valid functions

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module for encrypting passwords"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Returns hashed password"""
+
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str):
+    """Check i the password matches the hashed password"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
