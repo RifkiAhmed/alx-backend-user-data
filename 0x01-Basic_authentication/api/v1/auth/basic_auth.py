cede9a7ac9401cce7de8ec3a7c37cee66369dfc3
ahmed <rifkiahmed.91@gmail.com>
2024-02-13 03:28:24 +0100

    Create the method user_object_from_credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -61,5 +61,5 @@ class BasicAuth(Auth):
                 if user.is_valid_password(pwd=user_pwd):
                     return user
             return None
-        except Exception:
+        except IOError:
             return None
