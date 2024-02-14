43f2a462f00d7c287ef430328f38aec9dc880460
ahmed <rifkiahmed.91@gmail.com>
2024-02-14 21:44:55 +0100

    Add new route DELETE

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -2,7 +2,7 @@
 """ Module of SessionAuth views
 """
 from api.v1.views import app_views
-from flask import jsonify, request
+from flask import abort, jsonify, request
 from models.user import User
 from api.v1.auth.session_auth import SessionAuth
 import os
@@ -37,3 +37,16 @@ def login():
         session_name = os.getenv("SESSION_NAME")
         response.set_cookie(session_name, session_id)
         return response
+
+
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'],
+                 strict_slashes=False)
+def logout():
+    """ POST /api/v1/auth_session/login
+    User login
+    """
+    from api.v1.app import auth
+    if not auth.destroy_session(request):
+        abort(404)
+    return jsonify({}), 200
