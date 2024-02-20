1380d28d31fac2e127224f05895c2de08d628bb9
ahmed <rifkiahmed.91@gmail.com>
2024-02-20 01:46:44 +0100

    Add database module

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from typing import TypeVar
+from user import Base, User
+
+
+class DB:
+    """ DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> TypeVar("User"):
+        """Add User object to the database
+        Return
+        - User object
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
+
+    def find_user_by(self, **key: dict) -> TypeVar("User"):
+        """Returns User object based on the key argument
+        """
+        user = self._session.query(User).filter_by(**key).first()
+        if not user:
+            raise NoResultFound
+        return user
+
+    def update_user(self, user_id: int, **kwargs: dict) -> TypeVar("User"):
+        """Updates a User object with the user_id
+        """
+        try:
+            user = self.find_user_by(id=user_id)
+            if user:
+                for key, value in kwargs.items():
+                    setattr(user, key, value)
+            return None
+        except NoResultFound:
+            return None
