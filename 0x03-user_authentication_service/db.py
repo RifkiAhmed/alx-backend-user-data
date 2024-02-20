9c3612f92ea5456638163ae12acbe9dbaa66bc06
ahmed <rifkiahmed.91@gmail.com>
2024-02-20 19:07:12 +0100

    Fix check

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -40,7 +40,7 @@ class DB:
         self._session.commit()
         return user
 
-    def find_user_by(self, **kwargs: dict) -> User:
+    def find_user_by(self, **kwargs) -> User:
         """Returns User object based on the key argument
         """
         user = self._session.query(User).filter_by(**kwargs).one()
