981c7986907f222f04675b80424f787290f6eac3
ahmed <rifkiahmed.91@gmail.com>
2024-02-21 21:29:47 +0100

    Debug peer's code

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -61,10 +61,10 @@ def login() -> str:
 @app.route("/sessions", methods=["DELETE"], strict_slashes=False)
 def logout() -> None:
     """Handle a DELETE request to log out a user."""
+    user = None
     session_id = request.cookies.get("session_id")
-    if not session_id:
-        abort(403)
-    user = AUTH.get_user_from_session_id(session_id)
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
     if not user:
         abort(403)
     AUTH.destroy_session(user.id)
