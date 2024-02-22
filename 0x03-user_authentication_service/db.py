f3994586a07d421a4ded1ff5b4d6e4b2e3d374f4
ahmed <rifkiahmed.91@gmail.com>
2024-02-22 22:40:59 +0100

    Debugging peer's code

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -91,18 +91,8 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    # def add_user(self, email: str, hashed_password: str) -> User:
-    #     """Add new user to the database
-    #     """
-    #     user = User(email=email, hashed_password=hashed_password)
-    #     self._session.add(user)
-    #     self._session.commit()
-    #     return user
-
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add User object to the database
-        Return
-        - User object
         """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
