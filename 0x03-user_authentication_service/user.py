a27ab8cd8b050c9918847ec4f488df6808aace64
ahmed <rifkiahmed.91@gmail.com>
2024-02-22 22:43:01 +0100

    Debug peer's code

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -16,7 +16,6 @@
 #     hashed_password = Column(String(250), nullable=False)
 #     session_id = Column(String(250), nullable=True)
 #     reset_token = Column(String(250), nullable=True)
-
 '''User model'''
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -28,9 +27,9 @@ Base = declarative_base()
 class User(Base):
     '''User model
     '''
-    __tablename__ = "users"
+    __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
