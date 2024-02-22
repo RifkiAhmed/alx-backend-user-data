c243146b8520051b232955756643c91b910d417b
ahmed <rifkiahmed.91@gmail.com>
2024-02-22 22:50:04 +0100

    Debugging peer's code

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -96,6 +96,8 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add User object to the database
+        Return
+        - User object
         """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
